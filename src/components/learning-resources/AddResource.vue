508d33fd0f682c0112f0b4b888a99de5125070de
tomaszkoziolek <mes.tomaszkoziolek@gmail.com>
2024-06-17 16:13:20 +0200

    complete

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,27 +1,75 @@
 <template>
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid Input"
+    @close="confirmError"
+  >
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid.</p>
+      <p>Please check all inputs.</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form>
       <div class="form-controll">
         <label for="title">Title</label>
-        <input id="title" name="title" type="text" />
+        <input id="title" name="title" type="text" ref="titleInput" />
       </div>
       <div class="form-controll">
         <label for="description">Description</label>
-        <textarea name="description" id="description" rows="3"></textarea>
+        <textarea
+          name="description"
+          id="description"
+          rows="3"
+          ref="descInput"
+        ></textarea>
       </div>
       <div class="form-controll">
         <label for="link">Title</label>
-        <input id="link" name="link" type="url" />
+        <input id="link" name="link" type="url" ref="linkInput" />
       </div>
       <div>
-        <base-button type="submit">Add Resource</base-button>
+        <base-button type="submit" @click.prevent="setData"
+          >Add Resource</base-button
+        >
       </div>
     </form>
   </base-card>
 </template>
 
 <script>
-export default {};
+export default {
+  inject: ["addResource"],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setData() {
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredDescription = this.$refs.descInput.value;
+      const enteredLink = this.$refs.linkInput.value;
+
+      if (
+        enteredTitle.trim() === "" ||
+        enteredDescription.trim() === "" ||
+        enteredLink.trim() === ""
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
+      this.addResource(enteredTitle, enteredDescription, enteredLink);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,15 +1,17 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')"
-    :mode="storedResBtnMode"
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResBtnMode"
       >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTab('add-resource')"
-    :mode="addResBtnMode"
+    <base-button @click="setSelectedTab('add-resource')" :mode="addResBtnMode"
       >Add Resource</base-button
     >
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -42,20 +44,38 @@ export default {
   },
   computed: {
     storedResBtnMode() {
-        return this.selectedTab === 'stored-resources' ? null : 'flat';
+      return this.selectedTab === "stored-resources" ? null : "flat";
     },
     addResBtnMode() {
-        return this.selectedTab === 'add-resource' ? null : 'flat';
-    }
+      return this.selectedTab === "add-resource" ? null : "flat";
+    },
   },
   provide() {
     return {
-        resources: this.storedResources
-    }
+      resources: this.storedResources,
+      addResource: this.addResource,
+      deleteResource: this.removeResource,
+    };
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    addResource(title, description, link) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: link,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = "stored-resources";
+    },
+    removeResource(resId) {
+      const resIndex = this.storedResources.findIndex(
+        (res) => res.id === resId
+      );
+      this.storedResources.splice(resIndex, 1);
     },
   },
 };
